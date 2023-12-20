d86156b0aaa8e1f19299bd2f63cc3dceb53bb2bd
Michael Ilyin <milyin@gmail.com>
2023-12-20 14:39:52 +0100

    initial

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::sync::Arc;
+
+pub struct ArcTrace<T> {
+    arc: Arc<T>,
+}
+
+impl<T> ArcTrace<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            arc: Arc::new(value),
+        }
+    }
+}
+
+impl Clone for ArcTrace<T> {
+    fn clone(&self) -> Self {
+        Self {
+            arc: self.arc.clone(),
+        }
+    }
+}
+
+impl<T> std::ops::Deref for ArcTrace<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.arc
+    }
+}
