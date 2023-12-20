7e398d350c28fbb87943e4aaefc31ae807a3e07a
Michael Ilyin <milyin@gmail.com>
2023-12-20 15:55:39 +0100

    id, logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,61 @@
-use std::sync::Arc;
+use std::{sync::{Arc, atomic::AtomicU64}, any::type_name};
+
+pub struct Tracer {
+    name: String,
+    next_id: AtomicU64
+}
+
+impl Tracer {
+    pub fn new(
+        name: String,
+    ) -> Self {
+        Self {
+            name,
+            next_id: AtomicU64::new(0)
+        }
+    }
+    pub fn get_next_id(&self) -> u64 {
+        self.next_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+    }
+}
 
 pub struct ArcTrace<T> {
     arc: Arc<T>,
+    tracer: Arc<Tracer>,
+    id: u64,
 }
 
 impl<T> ArcTrace<T> {
     pub fn new(value: T) -> Self {
+        let arc = Arc::new(value);
+        let tracer = Arc::new(Tracer::new(type_name::<T>().to_string()));
+        let id = tracer.get_next_id();
+        log::warn!("{} {} {}", tracer.name, id, "created");
         Self {
-            arc: Arc::new(value),
+            arc,
+            tracer,
+            id,
         }
     }
 }
 
 impl<T> Clone for ArcTrace<T> {
     fn clone(&self) -> Self {
+        let arc = self.arc.clone();
+        let tracer = self.tracer.clone();
+        let id = tracer.get_next_id();
+        log::warn!("{} {} {}", tracer.name, id, "cloned");
         Self {
-            arc: self.arc.clone(),
+            arc,
+            tracer,
+            id,
         }
+    }
+}
+
+impl<T> Drop for ArcTrace<T> {
+    fn drop(&mut self) {
+        log::error!("{} {} {}", self.tracer.name, self.id, "dropped");
     }
 }
 
